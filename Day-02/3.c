121af86b8d05724b294d6d3bdc958772daf67f0e
Thumar Vrund <vrundthumar@gmail.com>
2025-07-16 20:19:41 +0530

    Update

--- Day-02/3.c
+++ Day-02/3.c
@@ -1,20 +1,20 @@
-// Write a C program to print @d array.
+// Write a C program to print 2d array.
 
 #include <stdio.h>
 void main() {
 	int a[5][5], i, j, rows, cols;
 	printf("Enter row and column sizes : ");
-	scanf("%d %d", &rows, &cols ); //Correct the code
-	for (i =0; i<rows ; i++) { //Correct the code
-		for (j=0; j<cols  ; j++  ) { //Correct the code
+	scanf("%d %d", &rows, &cols );
+	for (i =0; i<rows ; i++) { 
+		for (j=0; j<cols  ; j++  ) { 
 			printf("Enter the value of a[%d][%d] : ", i, j);
-			scanf("%d", &a[i][j]); //Correct the code
+			scanf("%d", &a[i][j]); 
 		}
 	}
 	printf("The given matrix is\n");
-	for (i=0; i<rows ; i++ ) { //Correct the code
-		for (j=0 ; j<cols ; j++ ) { //Correct the code
-			printf("%d ", a[i][j] ); //Correct the code
+	for (i=0; i<rows ; i++ ) { 
+		for (j=0 ; j<cols ; j++ ) {
+			printf("%d ", a[i][j] );
 		}
 		printf("\n");
 	}
